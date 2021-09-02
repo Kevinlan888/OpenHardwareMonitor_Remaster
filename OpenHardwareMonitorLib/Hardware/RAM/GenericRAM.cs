0e38f32e01851831811beb46907422b51a3c5268
kevin <kevin@kevinlan.top>
2021-09-02 14:17:26 +0800

    add self memory info

--- OpenHardwareMonitorLib/Hardware/RAM/GenericRAM.cs
+++ OpenHardwareMonitorLib/Hardware/RAM/GenericRAM.cs
@@ -8,73 +8,117 @@
 	
 */
 
+using System;
 using System.Runtime.InteropServices;
 
-namespace OpenHardwareMonitor.Hardware.RAM {
-  internal class GenericRAM : Hardware {
+namespace OpenHardwareMonitor.Hardware.RAM
+{
+    internal class GenericRAM : Hardware
+    {
+        private Sensor loadSensor;
+        private Sensor appLoadSensor;
+        private Sensor usedMemory;
+        private Sensor appUsedMemory;
+        private Sensor availableMemory;
 
-    private Sensor loadSensor;
-    private Sensor usedMemory;
-    private Sensor availableMemory;
+        public GenericRAM(string name, ISettings settings)
+          : base(name, new Identifier("ram"), settings)
+        {
+            loadSensor = new Sensor("Memory", 0, SensorType.Load, this, settings);
+            ActivateSensor(loadSensor);
 
-    public GenericRAM(string name, ISettings settings)
-      : base(name, new Identifier("ram"), settings)
-    {   
-      loadSensor = new Sensor("Memory", 0, SensorType.Load, this, settings);
-      ActivateSensor(loadSensor);
+            appLoadSensor = new Sensor("App used memory", 0, SensorType.Load, this, settings);
+            ActivateSensor(appLoadSensor);
 
-      usedMemory = new Sensor("Used Memory", 0, SensorType.Data, this,
-        settings);
-      ActivateSensor(usedMemory);
+            appUsedMemory = new Sensor("App used memory", 0, SensorType.SmallData, this, settings);
+            ActivateSensor(appUsedMemory);
 
-      availableMemory = new Sensor("Available Memory", 1, SensorType.Data, this, 
-        settings);
-      ActivateSensor(availableMemory);
+            usedMemory = new Sensor("Used Memory", 0, SensorType.Data, this,
+              settings);
+            ActivateSensor(usedMemory);
+
+            availableMemory = new Sensor("Available Memory", 1, SensorType.Data, this,
+              settings);
+            ActivateSensor(availableMemory);
+        }
+
+        public override HardwareType HardwareType
+        {
+            get
+            {
+                return HardwareType.RAM;
+            }
+        }
+
+        public override void Update()
+        {
+            NativeMethods.MemoryStatusEx status = new NativeMethods.MemoryStatusEx();
+            NativeMethods.PROCESS_MEMORY_COUNTERS processMemCnt = new NativeMethods.PROCESS_MEMORY_COUNTERS();
+            processMemCnt.cb = (uint)Marshal.SizeOf(typeof(NativeMethods.PROCESS_MEMORY_COUNTERS));
+
+            status.Length = checked((uint)Marshal.SizeOf(
+                typeof(NativeMethods.MemoryStatusEx)));
+
+            if (!NativeMethods.GlobalMemoryStatusEx(ref status))
+                return;
+
+            if (!NativeMethods.GetProcessMemoryInfo(System.Diagnostics.Process.GetCurrentProcess().Handle, out processMemCnt, processMemCnt.cb))
+                return;
+
+            loadSensor.Value = 100.0f -
+              (100.0f * status.AvailablePhysicalMemory) /
+              status.TotalPhysicalMemory;
+
+            appLoadSensor.Value = (100.0f * processMemCnt.WorkingSetSize) /
+              status.TotalPhysicalMemory;
+
+            appUsedMemory.Value = processMemCnt.WorkingSetSize / 1024 / 1024;
+
+            usedMemory.Value = (float)(status.TotalPhysicalMemory
+              - status.AvailablePhysicalMemory) / (1024 * 1024 * 1024);
+
+            availableMemory.Value = (float)status.AvailablePhysicalMemory /
+              (1024 * 1024 * 1024);
+        }
+
+        private class NativeMethods
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MemoryStatusEx
+            {
+                public uint Length;
+                public uint MemoryLoad;
+                public ulong TotalPhysicalMemory;
+                public ulong AvailablePhysicalMemory;
+                public ulong TotalPageFile;
+                public ulong AvailPageFile;
+                public ulong TotalVirtual;
+                public ulong AvailVirtual;
+                public ulong AvailExtendedVirtual;
+            }
+
+            [StructLayout(LayoutKind.Sequential, Size = 72)]
+            public struct PROCESS_MEMORY_COUNTERS
+            {
+                public uint cb;
+                public uint PageFaultCount;
+                public UInt64 PeakWorkingSetSize;
+                public UInt64 WorkingSetSize;
+                public UInt64 QuotaPeakPagedPoolUsage;
+                public UInt64 QuotaPagedPoolUsage;
+                public UInt64 QuotaPeakNonPagedPoolUsage;
+                public UInt64 QuotaNonPagedPoolUsage;
+                public UInt64 PagefileUsage;
+                public UInt64 PeakPagefileUsage;
+            }
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool GlobalMemoryStatusEx(
+              ref NativeMethods.MemoryStatusEx buffer);
+
+            [DllImport("psapi.dll", SetLastError = true)]
+            internal static extern bool GetProcessMemoryInfo(IntPtr hProcess, out PROCESS_MEMORY_COUNTERS counters, uint size);
+        }
     }
-
-    public override HardwareType HardwareType {
-      get {
-        return HardwareType.RAM;
-      }
-    }
-
-    public override void Update() {
-      NativeMethods.MemoryStatusEx status = new NativeMethods.MemoryStatusEx();
-      status.Length = checked((uint)Marshal.SizeOf(
-          typeof(NativeMethods.MemoryStatusEx)));
-
-      if (!NativeMethods.GlobalMemoryStatusEx(ref status))
-        return;
-
-      loadSensor.Value = 100.0f -
-        (100.0f * status.AvailablePhysicalMemory) /
-        status.TotalPhysicalMemory;
-
-      usedMemory.Value = (float)(status.TotalPhysicalMemory 
-        - status.AvailablePhysicalMemory) / (1024 * 1024 * 1024);
-
-      availableMemory.Value = (float)status.AvailablePhysicalMemory /
-        (1024 * 1024 * 1024);
-    }
-
-    private class NativeMethods {
-      [StructLayout(LayoutKind.Sequential)]
-      public struct MemoryStatusEx {
-        public uint Length;
-        public uint MemoryLoad;
-        public ulong TotalPhysicalMemory;
-        public ulong AvailablePhysicalMemory;
-        public ulong TotalPageFile;
-        public ulong AvailPageFile;
-        public ulong TotalVirtual;
-        public ulong AvailVirtual;
-        public ulong AvailExtendedVirtual;
-      }
-
-      [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-      [return: MarshalAs(UnmanagedType.Bool)]
-      internal static extern bool GlobalMemoryStatusEx(
-        ref NativeMethods.MemoryStatusEx buffer);
-    }
-  }
 }
